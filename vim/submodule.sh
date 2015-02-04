4e561f24997117b798e3d227cb43f50d8b9c7228
Patrick Ancillotti <patrick.ancillotti@clickbank.com>
2015-02-04 00:56:36 -0700

    Added submodule.sh

---
+++ vim/submodule.sh
@@ -0,0 +1,44 @@
+# Bundle 'gmarik/vundle'
+git submodule add -f https://github.com/gmarik/vundle.git vim/.vim/bundle/vundle
+
+# Bundle 'scrooloose/syntastic'
+git submodule add -f https://github.com/scrooloose/syntastic.git vim/.vim/bundle/syntastic
+
+# Bundle 'tpope/vim-pathogen'
+git submodule add -f https://github.com/tpope/vim-pathogen.git vim/.vim/bundle/vim-pathogen
+
+# Bundle 'tpope/vim-fugitive'
+git submodule add -f https://github.com/tpope/vim-fugitive.git vim/.vim/bundle/vim-fugitive
+
+# Bundle 'tpope/vim-surround'
+git submodule add -f https://github.com/tpope/vim-surround.git vim/.vim/bundle/vim-surround
+
+# Bundle 'Raimondi/delimitMate'
+git submodule add -f https://github.com/Raimondi/delimitMate.git vim/.vim/bundle/delimitMate
+
+# Bundle 'docunext/closetag.vim'
+git submodule add -f https://github.com/docunext/closetag.vim.git vim/.vim/bundle/closetag.vim
+
+# Bundle 'scrooloose/nerdcommenter'
+git submodule add -f https://github.com/scrooloose/nerdcommenter.git vim/.vim/bundle/nerdcommenter
+
+# Bundle 'majutsushi/tagbar'
+git submodule add -f https://github.com/majutsushi/tagbar.git vim/.vim/bundle/tagbar
+
+# Bundle 'sophacles/vim-bundle-sparkup'
+git submodule add -f https://github.com/sophacles/vim-bundle-sparkup.git vim/.vim/bundle/vim-bundle-sparkup
+
+# Bundle 'rodjek/vim-puppet'
+git submodule add -f https://github.com/rodjek/vim-puppet.git vim/.vim/bundle/vim-puppet
+
+# Bundle 'davidhalter/jedi-vim'
+git submodule add -f https://github.com/davidhalter/jedi-vim.git vim/.vim/bundle/jedi-vim
+
+# Bundle 'jeetsukumaran/vim-buffergator'
+git submodule add -f https://github.com/jeetsukumaran/vim-buffergator.git vim/.vim/bundle/vim-buffergator
+
+# Bundle 'digitaltoad/vim-jade'
+git submodule add -f https://github.com/digitaltoad/vim-jade.git vim/.vim/bundle/vim-jade
+
+# Bundle 'bling/vim-airline'
+git submodule add -f https://github.com/bling/vim-airline.git vim/.vim/bundle/vim-airline
